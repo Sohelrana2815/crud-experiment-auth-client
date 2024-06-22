ce0f3322c2f528f8b4a546013d7d17dd9a5b95f2
Sohelrana2815 <sohel152302@gmail.com>
2024-06-22 11:48:53 +0600

    main

--- src/Components/Providers/AuthProvider.jsx
+++ src/Components/Providers/AuthProvider.jsx
@@ -1,36 +1,27 @@
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { createContext } from "react";
 import auth from "../../FirebaseConfig/firebase-config";
 
-export const AuthContext = createContext(null)
-const AuthProvider = ({children}) => {
+export const AuthContext = createContext(null);
+const AuthProvider = ({ children }) => {
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
 
+  const signInUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
-
-
-
-const createUser = (email,password)=>{
-
-    return createUserWithEmailAndPassword(auth,email,password)
-
-}
-
-
-const signInUser=(email,password)=>{
-    return signInWithEmailAndPassword(auth,email,password)
-}
-
-
-
-     const authInfo={
-        createUser,
-        signInUser
-     }
-    return (
-        <AuthContext.Provider value={authInfo}>
-            {children}
-        </AuthContext.Provider>
-    );
+  const authInfo = {
+    createUser,
+    signInUser,
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;

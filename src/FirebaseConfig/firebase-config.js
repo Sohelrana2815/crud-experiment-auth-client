15326c52d19a8abbd9d1cb8fecafab8502ad7caa
Sohelrana2815 <sohel152302@gmail.com>
2024-06-22 12:16:21 +0600

    main

--- src/FirebaseConfig/firebase-config.js
+++ src/FirebaseConfig/firebase-config.js
@@ -6,15 +6,16 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCqErgtQciEuzLpkYQUbYvpx37MTa1zT1k",
-  authDomain: "experiment-crud-auth.firebaseapp.com",
-  projectId: "experiment-crud-auth",
-  storageBucket: "experiment-crud-auth.appspot.com",
-  messagingSenderId: "774632896950",
-  appId: "1:774632896950:web:17cd84e155e6f701d86917"
+  apiKey: "AIzaSyANqWeh1yXsXwyYKySqFEuGhjA7a36aYAg",
+  authDomain: "experiment-crud-auth-53955.firebaseapp.com",
+  projectId: "experiment-crud-auth-53955",
+  storageBucket: "experiment-crud-auth-53955.appspot.com",
+  messagingSenderId: "812122745472",
+  appId: "1:812122745472:web:00ab163c030a3c99a1d465",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app)
-export default auth
+const auth = getAuth(app);
+
+export default auth;
